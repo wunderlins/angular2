c91c46594f9df2b99e09dcb58033b058da7716b2
Simon Wunderlin <swunderlin@gmail.com>
2018-05-25 08:28:55 +0200

    working http client without error handling.

--- todo-client/src/app/app.module.ts
+++ todo-client/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { ConsoleComponent } from './console/console.component';
 import { NotesService } from './notes.service';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +14,8 @@ import { NotesService } from './notes.service';
     ConsoleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [NotesService],
   bootstrap: [AppComponent]
--- todo-client/src/app/notes.service.ts
+++ todo-client/src/app/notes.service.ts
@@ -2,11 +2,22 @@ import { Injectable } from '@angular/core';
 import { Todo } from './todo';
 import { Observable} from 'rxjs';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+/*
+const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*'
+  })
+};
+*/
 
 @Injectable()
 export class NotesService {
+  
+  private url = 'http://localhost:3000/nodes/0';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   
   private dummyData(): Todo {
     let n = new Todo(0, "Root");
@@ -20,7 +31,10 @@ export class NotesService {
   }
   
   getNotes(): Observable<Todo> {
-    return of(this.dummyData());
+    //return of(this.dummyData());
+    let root = this.http.get<Todo>(this.url);
+    console.log(root);
+    return root;
   }
 
 }
