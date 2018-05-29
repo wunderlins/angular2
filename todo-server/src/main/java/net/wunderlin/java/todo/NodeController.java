76de71d3c6f54dc99aed6a5c318f9c751d0bdd82
Simon Wunderlin <swunderlin@gmail.com>
2018-05-29 20:29:05 +0200

    added simple post handling for node

--- todo-server/src/main/java/net/wunderlin/java/todo/NodeController.java
+++ todo-server/src/main/java/net/wunderlin/java/todo/NodeController.java
@@ -2,12 +2,16 @@ package net.wunderlin.java.todo;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +29,7 @@ public class NodeController {
 		System.out.println("Database file: " + f);
 		try {
 			Database.open(f);
+			System.out.println("Opening Databse");
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 		}
@@ -76,5 +81,39 @@ public class NodeController {
         return children;
     }
     
-    
+    // TODO: add error handling
+    @PostMapping({"/node","/node/{id}"})
+	ResponseEntity<?> add(@PathVariable Integer id, @RequestBody Map<String, Object> input) {
+		//System.out.println("POST: " + Integer.toString(id));
+		//System.out.println(input);
+		int oid = (int) input.get("id");
+		Node n = new Node(oid);
+		n.setName((String) input.get("name"));
+		//System.out.println("Name: " + (String) input.get("name"));
+		//n.setDescription((String) input.get("description")); 
+		try {
+			n.store();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//System.out.println(n);
+		
+		/*
+		return this.accountRepository
+				.findByUsername(userId)
+				.map(account -> {
+					Bookmark result = bookmarkRepository.save(new Bookmark(account,
+							input.getUri(), input.getDescription()));
+
+					URI location = ServletUriComponentsBuilder
+						.fromCurrentRequest().path("/{id}")
+						.buildAndExpand(result.getId()).toUri();
+
+					return ResponseEntity.created(location).build();
+				})
+				.orElse(ResponseEntity.noContent().build());
+		*/
+		return ResponseEntity.noContent().build();
+	}
 }
